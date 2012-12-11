b21ce07e8bd15a10b316344bdac16cf1f9f614c5
Tajima Junpei <p.baleine@gmail.com>
2012-12-11 13:32:49 +0900

    add specs

---
+++ client/item-view/test/test.js
@@ -0,0 +1,87 @@
+describe('item-view', function() {
+  var ItemView = require('item-view');
+
+  beforeEach(function() {
+    this.model = new Backbone.Model({ title: 'hoge', done: false });
+    this.view = new ItemView({ model: this.model });
+  });
+
+  it('should be an instance of Backbone.View', function() {
+    this.view.should.be.an.instanceof(Backbone.View);
+  });
+
+  it('should have `item` as its class name', function() {
+    this.view.render().$el.should.have.class('item');
+  });
+
+  it('should have `li` as its tag name', function() {
+    this.view.render().el.tagName.should.be.eql('LI');
+  });
+
+  it('should render on `change` event of its model', function() {
+    var self = this;
+    (function() {
+      return self.view.$('label').length;
+    }).should.change.from(0).to(1)
+        .when(function() {
+          self.model.trigger('change');
+        });
+  });
+
+  it('should render title', function() {
+    this.view.render().$el.should.contain('hoge');
+  });
+
+  describe('`done` status', function() {
+    describe('when `done` is false', function() {
+      beforeEach(function() {
+        this.model = new Backbone.Model({ title: 'hoge', done: false });
+        this.view = new ItemView({ model: this.model });
+      });
+
+      it('should not check checkbox', function() {
+        this.view.render().$('[type=checkbox]').should.not.be.checked;
+      });
+    });
+
+    describe('when `done` is true', function() {
+      beforeEach(function() {
+        this.model = new Backbone.Model({ title: 'hoge', done: true });
+        this.view = new ItemView({ model: this.model });
+      });
+
+      it('should not check checkbox', function() {
+        this.view.render().$('[type=checkbox]').should.be.checked;
+      });
+    });
+  });
+
+  describe('when label is double clicked', function() {
+    beforeEach(function() {
+      this.view.render().$('.view').trigger('dblclick');
+    });
+
+    it('should be editable', function() {
+      this.view.$el.should.have.class('editing');
+    });
+  });
+
+  describe('when textfield receive enter keydown', function() {
+    beforeEach(function() {
+      this.saveSpy = sinon.spy(this.model, 'save');
+      this.event = Backbone.$.Event('keydown');
+      this.event.keyCode = this.event.which = 13;
+      this.view.$el.addClass('editing');
+      this.view.$('.edit').val('piyo').trigger(this.event);
+    });
+
+    afterEach(function() {
+      this.model.save.restore();
+    });
+
+    it('should save its title', function() {
+      this.saveSpy.should.have.been.called;
+      this.saveSpy.should.have.been.calledWith({ title: 'piyo' });
+    });
+  });
+});
---
+++ client/items/test/test.js
@@ -0,0 +1,20 @@
+describe('items', function() {
+  var Collection = require('items');
+
+  beforeEach(function() {
+    this.collection = new Collection([{ title: 'hoge' }]);
+  });
+
+  it('should be an instance of Backbone.Collection', function() {
+    this.collection.should.be.an.instanceof(Backbone.Collection);
+  });
+
+  it('should have `/items` as its url', function() {
+    this.collection.url.should.be.eql('/items');
+  });
+
+  it('should instantiate model with default value `done` with false', function() {
+    this.collection.should.have.length(1);
+    this.collection.first().get('done').should.be.equal(false);
+  });
+});
